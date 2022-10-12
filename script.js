cb5d8be74deb68004b928da13c9422024e1c87d0
Emanuel Joness <itzemanuelj@gmail.com>
2022-10-11 23:35:19 -0400

    mvp commit for math expression js

---
+++ script.js
@@ -0,0 +1,76 @@
+calculatedNum = 2 + (2 * 6);
+// What is the value of calculatedNum?
+14
+
+calculatedNum = (2 + 2) * 6;
+// What is the value of calculatedNum?
+24
+
+calculatedNum = (2 + 2) * (4 + 2);
+// 4 * 6 = 24
+// What is the value of calculatedNum?
+24
+
+calculatedNum = ((2 + 2) * 4) + 2;
+4 * 4 + 2 
+16 + 2
+// What is the value of calculatedNum?
+18
+
+// create a statement that assigns to num the result of 10 + 2 * 4, clarified with parentheses, producing 48.
+var num = (10 + 2) * 4;
+
+// Write a statement that assigns to num the result of 10 + 2 * 4, clarified with parentheses, producing 18. The variable hasn't been declared beforehand.
+var num = (4 * 2) + 10;
+
+// Write a statement that assigns to cost the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 56. The variable hasn't been declared beforehand.
+var cost = (2 + 2) * (4 + 10);
+
+// Write a statement that assigns to units the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 20. The variable hasn't been declared beforehand.
+var units = (4 * 2) + 10 + 2
+
+// Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing .5. The variable hasn't been declared beforehand.
+var pressure = 4 / (2 * 4);
+
+// Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing 8. The variable hasn't been declared beforehand.
+var pressure = 4 * (4 / 2);
+
+// Write a statement that assigns to a variable the result of adding two variables together and then afterward multiplying the result of the addition by another variable. The first variable has been declared beforehand.
+var sum = (time + cost) * multipy;
+
+// Write a statement that assigns to a variable the result of 4 / 2 * 4 - 1, clarified with parentheses, producing -.5. The variable hasn't been declared beforehand.
+var x = 4 / (2 * 4) - 1;
+
+// Write a statement that assigns to a variable the result of the following calculation: 2 numbers added together, with the result multiplied by a third number. The variable hasn't been declared beforehand.
+var num = (2 + 1) * 3;
+
+// Write a statement that assigns to units the result of 2 + 2 * 4 + 10, clarified with parentheses, producing 20. The variable hasn't been declared beforehand.
+var units = (2 * 4) + 2 + 10;
+
+// Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing .5. The variable hasn't been declared beforehand.
+var pressure = (4 * 2) / 4;
+
+// Write a statement that assigns to pressure the result of 4 / 2 * 4, clarified with parentheses, producing 8. The variable hasn't been declared beforehand.
+var pressure = (4 / 2) * 4;
+
+// Write a statement that assigns to a variable the result of adding two variables together and then afterward multiplying the result of the addition by another variable. The first variable has been declared beforehand.
+var nummy = (addy + add) * multy;
+
+// Write a statement that assigns to a variable the result of 4 / 2 * 4 - 1, clarified with parentheses, producing -.5. The variable hasn't been declared beforehand.
+var num = (4 / (2 * 4)) - 1;
+
+// Assign to a variable the result of a math expression that requires a set of parentheses.
+// Display an alert, specifying the variable as the message.
+// Click the Run It button to run your code live.
+// Type out the code. If you copy and paste it from the screen showing the correct code, it may not work.
+var nummy = (time + cost ) + 10;
+alert(nummy);
+
+// Assign numbers to three variables.
+// Display an alert, specifying as the message a math expression of your choosing that uses the three variables and requires parentheses. The alert will display the result of the calculation.
+
+var threeNumVar = (2 + 1) / 3;
+alert(threeNumVar);
+
+ 
+                            
